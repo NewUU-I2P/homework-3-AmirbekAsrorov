1e177edb700d39ca7a3b308b50e7525b814cffdc
AmirbekAsrorov <147371906+AmirbekAsrorov@users.noreply.github.com>
2023-11-10 14:11:48 +0500

    Update problem_3.cpp

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -3,7 +3,7 @@
 std::string problemSolution3(float height, char S) {
     // write your code here
     std::string result;
-    if (S == 'm') {
+    if (S == 'M') {
         if (height < 1.7) {
             result = "Short";
         } else if (height >= 1.7 && height < 1.85) {
@@ -11,7 +11,7 @@ std::string problemSolution3(float height, char S) {
         } else if (height >= 1.85) {
             result = "Tall";
         }
-    } else if (S == 'f') {
+    } else if (S == 'F') {
         if (height < 1.6) {
             result = "Short";
         } else if (height >= 1.6 && height < 1.75) {
