e82b94c62febe58c8830365b1470d9f7c82b921b
AmirbekAsrorov <147371906+AmirbekAsrorov@users.noreply.github.com>
2023-11-13 18:11:27 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -2,10 +2,6 @@ void problemSolution1(float consumed_water) {
     float cost;
     // write your code here
 
-        int main() {
-    double consumption =0;
-    cout<<" Enter the value of consumption\n";
-    cin>>consumption;
     double totalCost = calculateWaterCost(consumption);
     cout << "The total cost for a water consumption of " << consumption << " cubic meters is: " << totalCost << endl;
 
