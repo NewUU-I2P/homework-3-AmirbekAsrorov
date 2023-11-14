28377947c7cc76c1851a56c671e58b970e0f55e4
AmirbekAsrorov <147371906+AmirbekAsrorov@users.noreply.github.com>
2023-11-14 13:41:41 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -2,24 +2,24 @@ float problemSolution1(float consumed_water) {
     float cost;
     // write your code here
 
-    if (consumption <= 30.0) {
-        cost += consumption * 0.4;
-    } else if (consumption <= 50.0) {
-        cost += 30.0 * 0.4 + (consumption - 30.0) * 0.12;
-    } else if (consumption <= 60.0) {
-        cost += 30.0 * 0.4 + 20.0 * 0.12 + (consumption - 50.0) * 1.4;
+    if (consumed_water <= 30.0) {
+        cost += consumed_water * 0.4;
+    } else if (consumed_water <= 50.0) {
+        cost += 30.0 * 0.4 + (consumed_water - 30.0) * 0.12;
+    } else if (consumed_water <= 60.0) {
+        cost += 30.0 * 0.4 + 20.0 * 0.12 + (consumed_water - 50.0) * 1.4;
     } else {
-        cost += 30.0 * 0.4 + 20.0 * 0.12 + 10.0 * 1.4 + (consumption - 60.0) * 1.5;
+        cost += 30.0 * 0.4 + 20.0 * 0.12 + 10.0 * 1.4 + (consumed_water - 60.0) * 1.5;
     }
 
     return cost;
 }
 
 int main() {
-    double consumption =0;
-    cin>>consumption;
-    double totalCost = calculateWaterCost(consumption);
-    cout << "The total cost for a water consumption of " << consumption << " cubic meters is: " << totalCost << endl;
+    double consumed_water =0;
+    cin>>consumed_water;
+    double totalCost = calculateWaterCost(consumed_water);
+    cout << "The total cost for a water consumed_water of " << consumed_water << " cubic meters is: " << totalCost << endl;
 
     return 0;
 }
