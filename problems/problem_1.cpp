cb1300771f2302d83df920e7af40dbacdf49cbdc
AmirbekAsrorov <147371906+AmirbekAsrorov@users.noreply.github.com>
2023-11-14 15:30:23 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,7 +1,7 @@
 float problemSolution1(float consumed_water) {
     float cost;
     // write your code here
-
+/*
     if (consumed_water <= 30.0) {
         cost = consumed_water * 0.4;
     } else if (consumed_water <= 50.0) {
@@ -11,6 +11,6 @@ float problemSolution1(float consumed_water) {
     } else {
         cost = 30.0 * 0.4 + 20.0 * 0.12 + 10.0 * 1.4 + (consumed_water - 60.0) * 1.5;
     }
-
+*/
     return cost;
 }
